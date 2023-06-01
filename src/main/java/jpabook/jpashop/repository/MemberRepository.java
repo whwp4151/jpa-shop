057f7511e12022f240547f4cfda2a4710712886a
whwp4151 <whwp4151@naver.com>
2023-06-01 19:55:35 +0900

    feat: 회원 도메인 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,10 +1,13 @@
-package jpabook.jpashop;
+package jpabook.jpashop.repository;
 
+import java.util.List;
 import jpabook.jpashop.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByName(String name);
 
 }
